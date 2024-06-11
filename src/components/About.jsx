bd14f800994745aff22baf1b326924b2e2752518
sameera733 <sameerabegum324@gmail.com>
2024-06-11 08:24:38 +0530

    partial portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500"> About </p>
+            </div>
+
+            <p className="text-xl mt-20">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptatum cum inventore corrupti aspernatur magni laborum reprehenderit provident eligendi! Velit possimus doloremque, ex deserunt nesciunt nemo sint aut sit asperiores fuga iure officia sapiente labore amet impedit, esse repellendus, sequi ipsam recusandae error. Necessitatibus laborum vitae porro iure ducimus ipsum.
+            </p>
+
+            <br />
+
+            <p className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo omnis recusandae aspernatur dolorum incidunt fugiat ullam corporis accusantium eaque, est fuga, nisi, dolorem dolore amet officia. Maxime, temporibus ratione dolorem inventore aspernatur obcaecati assumenda a ad sapiente sit adipisci harum? Architecto voluptas distinctio laborum veniam nemo nam, harum asperiores nulla.
+            </p>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default About
